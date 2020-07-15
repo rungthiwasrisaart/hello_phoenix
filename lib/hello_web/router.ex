d53e2efd3c8c006a89e47a33860e83f6eff9de2e
rungthiwasrisaart <jamesung@DESKTOP-VGOJVB6.localdomain>
2020-07-15 16:43:36 +0700

    use Routing

--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -20,6 +20,33 @@ defmodule HelloWeb.Router do
     get "/", PageController, :index
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
+
+    resources "/users", UserController do
+      resources "/posts", PostController
+    end
+
+    resources "/posts", PostController, only: [:index, :show]
+    resources "/comments", CommentController, except: [:delete]
+    resources "/reviews", ReviewController
+    forward "/jobs", BackgroundJob.Plug, name: "Hello Phoenix"
+  end
+
+  scope "/admin", HelloWeb.Admin, as: :admin do
+    pipe_through :browser
+
+    resources "/images", ImageController
+    resources "/reviews", ReviewController
+    resources "/users", UserController
+  end
+
+  scope "/api", HelloWeb.Api, as: :api do
+    pipe_through :api
+
+    scope "/v1", V1, as: :v1 do
+      resources "/images", ImageController
+      resources "/reviews", ReviewController
+      resources "/users", UserController
+    end
   end
 
   # Other scopes may use custom stacks.
