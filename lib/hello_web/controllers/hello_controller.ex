8d64ebbb3156962c8c7fa38416b4b3cd79875e40
rungthiwasrisaart <jamesung@DESKTOP-VGOJVB6.localdomain>
2020-07-15 15:37:22 +0700

    use plug introspect and Locale

--- lib/hello_web/controllers/hello_controller.ex
+++ lib/hello_web/controllers/hello_controller.ex
@@ -1,5 +1,6 @@
 defmodule HelloWeb.HelloController do
   use HelloWeb, :controller
+  plug HelloWeb.Plugs.Locale, "en" when action in [:index]
 
   def index(conn, _params) do
     render(conn, "index.html")
---
+++ lib/hello_web/plugs/locale.ex
@@ -0,0 +1,16 @@
+defmodule HelloWeb.Plugs.Locale do
+  import Plug.Conn
+
+  @locales ["en", "fr", "de"]
+
+  def init(default), do: default
+
+  def call(%Plug.Conn{params: %{"locale" => loc}} = conn, _default) when loc in @locales do
+    assign(conn, :locale, loc)
+  end
+
+  def call(conn, default) do
+    IO.puts(default)
+    assign(conn, :locale, default)
+  end
+end
